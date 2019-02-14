51d5e6da216270a7a4f4128b64c6dadeb3b527c7
Pallav Jha <pallavjha25@gmail.com>
2019-02-13 20:41:51 -0700

    Commit from Eclipse

---
+++ OpenTradingSystem/src/MainClass.java
@@ -0,0 +1,25 @@
+public class MainClass {
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("Hello World from OTS");
+		
+		//Connect Oracle DB.
+		DictionaryClass dao = new DictionaryClass();
+		dao.openDBConnection();
+		
+		//Write a method string GetValue(string type, string key) which will return value 
+		//when dictionary Type and Key are passed to the method.
+		
+		//Calling by hard-coded values.
+		System.out.println(dao.GetValueFromTypeAndKey("TraderBookMapping", "anchal"));
+
+		//Write a method Dictionary<string,string> GetDictionary(string type) which will return complete dictionary (all key, value pairs) if passed a dictionary type.
+		dao.GetDictionaryFromType("TraderBookMapping");
+		
+		//Write a method Dictionary<string,Dictionary<string,string>> GetAllDictionaries(string type) which will return all dictionaries.		
+		dao.GetAllDictionaryAvailable();
+		
+		//Close DB Connection
+		dao.closeDbConnection();
+	}
+}
